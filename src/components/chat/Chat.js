9a73549f6b26346f0842ec2f004e2514ce032bf2
Steve Dongich <stevowny@gmail.com>
2024-09-25 22:44:00 +0300

    addd the query string to chat component and init socket.io-client

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,7 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import queryString from 'query-string'
+import { useLocation } from 'react-router-dom'
+import io from 'socket.io-client'
 import './Chat.css'
 
 const Chat = () => {
+  const location = useLocation()
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  useEffect(()=>{
+    const {name, room} = queryString.parse(location.search)
+    setName(name)
+    setRoom(room)
+  })
   return (
     <div>Chat</div>
   )
